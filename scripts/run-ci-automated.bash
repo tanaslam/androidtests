c1166a47694a71711835ac1c8705e1f7bb0ef363
Tanveer Aslam <tan.aslam@gmail.com>
2014-07-25 22:48:51 +0100

    Added avd command-line arg

--- scripts/run-ci-automated.bash
+++ scripts/run-ci-automated.bash
@@ -4,9 +4,17 @@
 # The script launches an already configured android virtual device (AVD)
 # and run connected tests on it 
 
+#Check if valid android virtual device name is provided
+if [ -z "$1" ]
+then
+	echo "You must provide valid android virtual device name:"
+	android list avd
+	exit
+fi 
+
 # Run emulator in separate and return 
-echo "Starting emulator..."
-emulator -no-window -noaudio -no-boot-anim @Nexus_4_by_Google&
+echo "Starting emulator @$1 virtual device..."
+emulator -no-window -noaudio -no-boot-anim @$1 &
 
 # Make adb to wait for device to become online
 echo "Waiting to emulator to become online..."
